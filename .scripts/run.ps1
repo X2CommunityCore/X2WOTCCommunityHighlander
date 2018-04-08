69c03c07f08100b6a442239ed7fe012e207b5886
robojumper <robojumper@gmail.com>
2018-04-08 17:24:18 +0200

    update run.ps1

--- .scripts/run.ps1
+++ .scripts/run.ps1
@@ -2,11 +2,4 @@ Param(
     [string]$gamePath # the path to your XCOM 2 installation ending in "XCOM 2"
 )
 
-# TODO: find out why i can't do this and i have to use the stupid batch file
-# Start-Process `
-#     -FilePath "$gamePath/Binaries/Win64/Launcher/ModLauncherWPF.exe" `
-#     -ArgumentList "-allowconsole", "-log", "-autodebug" `
-#     -NoNewWindow `
-#     -Wait
-
-Start-Process -FilePath "$gamePath/Binaries/Win64/Launcher/StartDebugging.bat" -Wait -NoNewWindow -WorkingDirectory "$gamePath/Binaries/Win64/Launcher"
+Start-Process -FilePath "$gamePath/Binaries/Win64/XCom2.exe" -ArgumentList "-fromlauncher -log -nostartupmovies -allowconsole" -Wait -WorkingDirectory "$gamePath/Binaries/Win64"
