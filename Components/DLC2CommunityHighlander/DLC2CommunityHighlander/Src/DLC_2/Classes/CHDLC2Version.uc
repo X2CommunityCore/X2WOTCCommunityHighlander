501ecaafe374e02c4c7c78036e33d1ec4a51d48a
Iridar <ps2guides@gmail.com>
2023-08-22 08:14:24 +0600

    Bump version to 1.26.2

--- Components/DLC2CommunityHighlander/DLC2CommunityHighlander/Src/DLC_2/Classes/CHDLC2Version.uc
+++ Components/DLC2CommunityHighlander/DLC2CommunityHighlander/Src/DLC_2/Classes/CHDLC2Version.uc
@@ -11,6 +11,6 @@ defaultproperties
 {
 	MajorVersion = 1;
 	MinorVersion = 26;
-	PatchVersion = 1;
+	PatchVersion = 2;
 	Commit = "";
 }
--- VERSION.ps1
+++ VERSION.ps1
@@ -6,7 +6,7 @@ defaultproperties
 {
 	MajorVersion = 1;
 	MinorVersion = 26;
-	PatchVersion = 1;
+	PatchVersion = 2;
 	Commit = "%COMMIT%";
 }
 '@
